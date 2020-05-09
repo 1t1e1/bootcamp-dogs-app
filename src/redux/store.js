edad3c3a2a3862b47fe8b0211186e2814b8c9f76
1t1e1 <1t1ebir@gmail.com>
2020-05-10 00:43:53 +0300

    redux dev tool eklendi.

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,4 +5,8 @@
 import { rootReducer } from "./reducer";
 import { createStore } from "redux";
 
-export const store = createStore(rootReducer);
+export const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
