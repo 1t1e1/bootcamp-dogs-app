8ba62904b3f5b59f6e67172daa47fdd8473b9d68
1t1e1 <1t1ebir@gmail.com>
2020-06-22 20:13:21 +0300

    Apiconfig and edit for deployment

--- src/App.js
+++ src/App.js
@@ -8,41 +8,38 @@ import FilteredDogs from "./containers/FilteredDogs";
 import DogProfile from "./containers/DogProfile";
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <Header />
-                <Switch>
-                    <Route path="/" exact component={Homepage} />
-                    <Route
-                        path="/hakkinda"
-                        exact
-                        component={() => <div>Hakkimizda Componenti</div>}
-                    />
-                    <Route
-                        path="/tur/:yazilanTur"
-                        exact
-                        component={FilteredDogs}
-                    />
-                    <Route path="/detail/:dogId" exact component={DogProfile} />
-                    <Route
-                        component={() => (
-                            <div>
-                                {" "}
-                                <h1>404 sayfa bulununamadi</h1>{" "}
-                            </div>
-                        )}
-                    />
-                    {/*{*/}
-                    {/*    routes.map((route) => {*/}
-                    {/*        // <Route {...route} />*/}
-                    {/*        return <Route key={route.path} path={route.path} exact={route.exact} component={route.component} />*/}
-                    {/*    })*/}
-                    {/*}*/}
-                </Switch>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<Switch>
+					<Route path="/" exact component={Homepage} />
+					<Route path="/bootcamp-dogs-app" exact component={Homepage} />
+					<Route
+						path="/hakkinda"
+						exact
+						component={() => <div>Hakkimizda Componenti</div>}
+					/>
+					<Route path="/tur/:yazilanTur" exact component={FilteredDogs} />
+					<Route path="/detail/:dogId" exact component={DogProfile} />
+					<Route
+						component={() => (
+							<div>
+								{" "}
+								<h1>404 sayfa bulununamadi</h1>{" "}
+							</div>
+						)}
+					/>
+					{/*{*/}
+					{/*    routes.map((route) => {*/}
+					{/*        // <Route {...route} />*/}
+					{/*        return <Route key={route.path} path={route.path} exact={route.exact} component={route.component} />*/}
+					{/*    })*/}
+					{/*}*/}
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/personalConfig.js
@@ -0,0 +1,3 @@
+const url = "https://5ea5688b2d86f00016b45bcf.mockapi.io/favorites";
+
+export default url;
